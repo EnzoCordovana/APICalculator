5f32ee243685e3147e9e2e3400ff19f471955215
EnzoCordovana <enzo.cordovana@etu.univ-amu.fr>
2025-07-21 19:04:07 +0200

    feat: ajout de la doc avec swagger

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,5 +1,13 @@
 use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
 use std::sync::Mutex;
+use utoipa::{OpenApi, ToSchema};
+use utoipa_swagger_ui::SwaggerUi;
+
+#[derive(ToSchema)]
+struct AppInfo {
+    app_name: String,
+    version: String,
+}
 
 struct AppState {
     app_name: String,
@@ -7,21 +15,31 @@ struct AppState {
     visit_count: Mutex<u32> // compteur protégé par Mutex, thread-safe 
 }
 
+#[utoipa::path(
+    get,
+    path = "/",
+    responses((status = 200, description = "Accueil")),
+    tag = "API Calculator"
+)]
 #[get("/")]
 async fn index(data: web::Data<AppState>) -> impl Responder {
     let mut count = data.visit_count.lock().unwrap();
     *count += 1;
-
     let message = format!(
         "Bienvenue sur {} (v{})\nNombre de visites: {}",
         data.app_name,
         data.version,
         count
     );
-
     HttpResponse::Ok().body(message)
 }
 
+#[utoipa::path(
+    get,
+    path = "/status",
+    responses((status = 200, description = "Status")),
+    tag = "API Calculator"
+)]
 #[get("/status")]
 async fn status(data: web::Data<AppState>) -> impl Responder {
     let message = format!(
@@ -29,12 +47,21 @@ async fn status(data: web::Data<AppState>) -> impl Responder {
         data.app_name,
         data.version
     );
-
     HttpResponse::Ok().body(message)
 }
 
+#[derive(OpenApi)]
+#[openapi(
+    paths(index, status),
+    components(schemas(AppInfo)),
+    tags((name = "API Calculator", description = "API pour calculatrice"))
+)]
+struct ApiDoc;
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    let addr: &str = "0.0.0.0";
+    let port: u16 = 8080;
 
     let app_state = web::Data::new(AppState {
         app_name: "API Calculator".to_string(),
@@ -45,11 +72,13 @@ async fn main() -> std::io::Result<()> {
     // Lancement du server
     HttpServer::new(move || {
         App::new()
+            .service(SwaggerUi::new("/doc/{_:.*}")
+                .url("/api-docs/openapi.json", ApiDoc::openapi()))
             .app_data(app_state.clone())
             .service(index)
             .service(status)
     })
-    .bind(("0.0.0.0", 8080))?
+    .bind((addr, port))?
     .run()
     .await
 }
